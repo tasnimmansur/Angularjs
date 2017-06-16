c6a5136df251a92c4a49c4b4d27a5af40a926f35
Ajeet Singh <ajeetsingh14121971@gmail.com>
2017-06-16 16:39:35 +0530

    "new file"

---
+++ nodeangular/pubilc/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('myApp', [
+    'ui.router'
+]).config(function($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider.state('app', {
+        url: '/',
+        templateUrl: 'app/app.html',
+        controller: 'AppCtrl'
+    });
+
+}).run(function () {
+
+}); 
---
+++ nodeangular/pubilc/app/app.controller.js
@@ -0,0 +1,3 @@
+angular.module('myApp').controller('AppCtrl', function($scope) {
+    $scope.message = 'Hello World!';
+});
